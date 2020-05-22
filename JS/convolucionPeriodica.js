4687a534029960aa2658af361b0a4034d5650b73
Silvestre <emmayuel80@gmail.com>
2020-05-22 14:21:49 -0500

    nerf circular, buff periodica

--- JS/convolucionPeriodica.js
+++ JS/convolucionPeriodica.js
@@ -23,7 +23,7 @@ function convolucionPeriodica(x,h,origen,periodo){
             result[g] += splitResults[i][g];
         }
     }
-    let finalArray = [0,0,0];
+    let finalArray = [0,0,0,0,0,0];
     resultado.vector = finalArray.fill(result).flat();
     resultado.origen = resultado.vector[resultado.origenIndex];
     console.table(resultado);
